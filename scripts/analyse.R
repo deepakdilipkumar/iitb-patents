cf6df523fdc97a387668d260128e47a646a48122
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-06-07 22:52:15 +0530

    Splitting into Indian/International

--- scripts/analyse.R
+++ scripts/analyse.R
@@ -7,3 +7,32 @@ library(readxl)
  other <- read_excel("..//data//patents.xlsx",5)
  granted <- read_excel("..//data//patents.xlsx",6,skip=1)
 
+index <- match("International Patent filed",ee[,1])
+ee <- ee[-index,1:6]
+eeInd <- ee[1:index-1,]
+eeInt <- ee[index:dim(ee)[1],]
+
+index <- match("International Patent filed",health[,1])
+health <- health[-index,1:6]
+healthInd <- health[1:index-1,]
+healthInt <- health[index:dim(health)[1],]
+
+index <- match("International Patent filed",ict[,1])
+ict <- ict[-index,1:6]
+ictInd <- ict[1:index-1,]
+ictInt <- ict[index:dim(ict)[1],]
+
+index <- match("International Patent filed",manchem[,1])
+manchem <- manchem[-index,1:6]
+manchemInd <- manchem[1:index-1,]
+manchemInt <- manchem[index:dim(manchem)[1],]
+
+index <- match("International Patent filed",other[,1])
+other <- other[-index,1:6]
+otherInd <- other[1:index-1,]
+otherInt <- other[index:dim(other)[1],]
+
+index <- match("International Patent Grant",granted[,1])
+granted <- granted[-index,1:6]
+grantedInd <- granted[1:index-1,]
+grantedInt <- granted[index:dim(granted)[1],]
